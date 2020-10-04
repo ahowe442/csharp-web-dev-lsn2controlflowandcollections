33bfbe097f991eebbc38d5c64aa9d8c5a0b919b8
Abby Howe <howe.abigail@gmail.com>
2020-10-04 13:27:21 -0500

    solved the Arravy Exercises.

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -7,6 +7,30 @@ namespace ArrayExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Arrays!");
+            //Creates and initializes an array with the following values in a single line:
+            int[] ints = { 1, 1, 2, 3, 5, 8 };
+
+            //Loops through the array and print out each value.
+            foreach (int i in ints)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            //Modiies the loop to only print the odd numbers from the array.
+            foreach (int i in ints)
+            {
+                //Takes the integer and divides it by 2 (modulo), if the result equals zero then it prints the integer.
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
         }
     }
 }
